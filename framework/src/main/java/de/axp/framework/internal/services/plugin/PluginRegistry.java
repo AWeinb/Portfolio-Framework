ac50f4b3e97dbfc1b9c23db3a697ca2469269be3
AWeinb <AWeinb@t-online.de>
2018-09-15 17:30:14 +0200

    Remove plugin scanning

--- framework/src/main/java/de/axp/framework/internal/services/plugin/PluginRegistry.java
+++ framework/src/main/java/de/axp/framework/internal/services/plugin/PluginRegistry.java
@@ -1,23 +1,12 @@
 package de.axp.framework.internal.services.plugin;
 
 import de.axp.framework.api.FrameworkPlugin;
-import de.axp.framework.api.plugins.DataHandler;
-import de.axp.framework.api.plugins.TaskHandler;
 
 import java.util.*;
 
 class PluginRegistry {
 
 	private final Map<Class<?>, Set<FrameworkPlugin>> plugins = new HashMap<>();
-
-	PluginRegistry() {
-		initPluginsOfType(DataHandler.class);
-		initPluginsOfType(TaskHandler.class);
-	}
-
-	private void initPluginsOfType(Class<? extends FrameworkPlugin> type) {
-		PluginScanner.getPlugins(type).forEach(p -> this.addPlugin(type, p));
-	}
 
 	void addPlugin(Class<? extends FrameworkPlugin> type, FrameworkPlugin frameworkPlugin) {
 		if (!plugins.containsKey(type)) {
