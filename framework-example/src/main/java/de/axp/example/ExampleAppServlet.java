843d113155e9cc5899c8b067f54c2c862272e246
AWeinb <AWeinb@t-online.de>
2018-09-27 19:44:36 +0200

    Improve example app boilerplate code

--- framework-example/src/main/java/de/axp/example/ExampleAppServlet.java
+++ framework-example/src/main/java/de/axp/example/ExampleAppServlet.java
@@ -1,10 +1,10 @@
-package de.axp.px;
-
-import javax.servlet.ServletException;
+package de.axp.example;
 
 import de.axp.portfolio.vaadin.PortfolioVaadinServlet;
 
-public class FooServlet extends PortfolioVaadinServlet {
+import javax.servlet.ServletException;
+
+public class ExampleAppServlet extends PortfolioVaadinServlet {
 
 	private static final long serialVersionUID = -713474004104403527L;
 
