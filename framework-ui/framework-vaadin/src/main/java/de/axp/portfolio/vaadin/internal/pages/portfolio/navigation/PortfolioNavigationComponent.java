7ee6a4664e9acb24514d247a0fd8c13dd72ddc5d
AWeinb <AWeinb@t-online.de>
2018-10-16 19:45:17 +0200

    Move common navigation style to base style

--- framework-ui/framework-vaadin/src/main/java/de/axp/portfolio/vaadin/internal/pages/portfolio/navigation/PortfolioNavigationComponent.java
+++ framework-ui/framework-vaadin/src/main/java/de/axp/portfolio/vaadin/internal/pages/portfolio/navigation/PortfolioNavigationComponent.java
@@ -1,7 +1,9 @@
 package de.axp.portfolio.vaadin.internal.pages.portfolio.navigation;
 
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.html.Nav;
 
+@StyleSheet("frontend://styles/portfolio-navigation.css")
 public abstract class PortfolioNavigationComponent extends Nav {
 
 	private static final long serialVersionUID = 1614357453838016041L;
