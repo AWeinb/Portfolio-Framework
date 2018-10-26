ca05a0f53bc1777914f9c1119c75d6fa792103b9
AWeinb <AWeinb@t-online.de>
2018-10-26 20:10:58 +0200

    Introduce base div for all contents and adjust styles

--- framework-ui/framework-vaadin/src/main/java/de/axp/portfolio/vaadin/internal/pages/portfolio/content/PortfolioContentManager.java
+++ framework-ui/framework-vaadin/src/main/java/de/axp/portfolio/vaadin/internal/pages/portfolio/content/PortfolioContentManager.java
@@ -1,6 +1,7 @@
 package de.axp.portfolio.vaadin.internal.pages.portfolio.content;
 
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.html.Div;
 import de.axp.framework.api.services.UiService;
 import de.axp.framework.api.services.UiService.PortfolioPart;
 import de.axp.portfolio.vaadin.internal.pages.portfolio.PortfolioPageState;
@@ -9,14 +10,18 @@ import de.axp.portfolio.vaadin.internal.pages.portfolio.content.simple.SimpleCon
 public class PortfolioContentManager {
 
 	private final PortfolioPageState pageState;
+	private final Div contentBase = new Div();
 	private final SimpleContentLayout simpleContentLayout = new SimpleContentLayout();
 
 	public PortfolioContentManager(PortfolioPageState pageState) {
 		this.pageState = pageState;
+
+		contentBase.setClassName("content-base");
+		contentBase.add(simpleContentLayout);
 	}
 
-	public PortfolioContentComponent getContentComponent() {
-		return simpleContentLayout;
+	public Component getContentComponent() {
+		return contentBase;
 	}
 
 	@SuppressWarnings("unchecked")
