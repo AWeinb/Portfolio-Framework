4b9286fa6c9952bb0f3cc5219eef9280160c2506
AWeinb <AWeinb@t-online.de>
2018-10-28 13:41:49 +0100

    Reduce content logic

--- framework-ui/framework-vaadin/src/main/java/de/axp/portfolio/vaadin/internal/pages/portfolio/content/PortfolioContentComponent.java
+++
@@ -1,9 +0,0 @@
-package de.axp.portfolio.vaadin.internal.pages.portfolio.content;
-
-import com.vaadin.flow.component.dependency.StyleSheet;
-import com.vaadin.flow.component.html.Section;
-
-@StyleSheet("frontend://styles/portfolio-content.css")
-public abstract class PortfolioContentComponent extends Section {
-
-}
--- framework-ui/framework-vaadin/src/main/java/de/axp/portfolio/vaadin/internal/pages/portfolio/content/PortfolioContentManager.java
+++ framework-ui/framework-vaadin/src/main/java/de/axp/portfolio/vaadin/internal/pages/portfolio/content/PortfolioContentManager.java
@@ -2,35 +2,25 @@ package de.axp.portfolio.vaadin.internal.pages.portfolio.content;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.html.Div;
-import de.axp.framework.api.services.UiService;
-import de.axp.framework.api.services.UiService.PortfolioPart;
 import de.axp.portfolio.vaadin.internal.pages.portfolio.PortfolioPageState;
-import de.axp.portfolio.vaadin.internal.pages.portfolio.content.simple.SimpleContentLayout;
 
 public class PortfolioContentManager {
 
-	private final PortfolioPageState pageState;
 	private final Div contentBase = new Div();
-	private final SimpleContentLayout simpleContentLayout = new SimpleContentLayout();
+	private final PortfolioContent portfolioContent;
 
 	public PortfolioContentManager(PortfolioPageState pageState) {
-		this.pageState = pageState;
+		portfolioContent = new PortfolioContent(pageState);
 
 		contentBase.setClassName("content-base");
-		contentBase.add(simpleContentLayout);
+		contentBase.add(portfolioContent);
 	}
 
 	public Component getContentComponent() {
 		return contentBase;
 	}
 
-	@SuppressWarnings("unchecked")
 	public void update() {
-		simpleContentLayout.removeAll();
-
-		UiService.PortfolioDefinition portfolioDefinition = pageState.getPortfolioDefinition();
-		int portfolioPartIndex = pageState.getPortfolioPartIndex();
-		PortfolioPart<Component> part = portfolioDefinition.getPortfolioParts().get(portfolioPartIndex);
-		simpleContentLayout.add(part.getUiComponent());
+		portfolioContent.update();
 	}
 }
