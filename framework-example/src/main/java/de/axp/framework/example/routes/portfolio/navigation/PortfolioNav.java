6461430119db15a41f9f4971dd6ae0f3289800a7
AWeinb <AWeinb@t-online.de>
2018-11-20 19:42:58 +0100

    Style new navigation elements

--- framework-example/src/main/java/de/axp/framework/example/routes/portfolio/navigation/PortfolioNav.java
+++ framework-example/src/main/java/de/axp/framework/example/routes/portfolio/navigation/PortfolioNav.java
@@ -1,9 +1,11 @@
 package de.axp.framework.example.routes.portfolio.navigation;
 
 import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Nav;
 
+@StyleSheet("frontend://styles/portfolio-navigation.css")
 public class PortfolioNav extends Nav {
 
 	public PortfolioNav() {
